c96bc37e3b27744565bc82674d9dbac9160c6cab
Jędrzej Karolonek <jkarolonek@n-educatio.pl>
2018-08-06 15:51:54 +0200

    initial canvas manipulation

--- src/app/index.js
+++ src/app/index.js
@@ -1,1 +1,16 @@
+import '../style/app.scss';
 
+document.addEventListener('DOMContentLoaded', (event) => {
+  const snakeBoard = document.getElementById('snake-board');
+  const ctx = snakeBoard.getContext('2d');
+
+  const tileSize = 25;
+  const tileX = 20;
+  const tileY = 20;
+
+  for (let x = 0; x < tileX; x++) {
+    for (let y = 0; y < tileY; y++) {
+      ctx.strokeRect(x, y, tileSize, tileSize);
+    }
+  }
+});
