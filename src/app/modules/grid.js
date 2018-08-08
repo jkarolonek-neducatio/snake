77c602f7a114974a65b856bcbd400a3febd2e063
Jędrzej Karolonek <jkarolonek@n-educatio.pl>
2018-08-08 13:42:59 +0200

    added bonus generation, segment addition on bonus collection, clear interval on snake overlap and out of bounds snake move, bug on some refreshes crashes browser (for investigation)

--- src/app/modules/grid.js
+++ src/app/modules/grid.js
@@ -6,6 +6,7 @@ class Grid {
     this.tileSize = tileSize;
     this.context = this.canvas.getContext('2d');
     this.direction = 'down';
+    this.bonus = false;
     this.snake = [[10, 10], [11, 10], [12, 10], [13, 10], [14, 10]];
     this.update();
     window.addEventListener('keydown', this.keydownHandler);
@@ -13,15 +14,19 @@ class Grid {
 
   keydownHandler = (event) => {
     switch (event.keyCode) {
+      case 65:
       case 37:
         this.direction = 'left';
         break;
+      case 87:
       case 38:
         this.direction = 'up';
         break;
+      case 68:
       case 39:
         this.direction = 'right';
         break;
+      case 83:
       case 40:
         this.direction = 'down';
         break;
@@ -41,7 +46,7 @@ class Grid {
   }
 
   update() {
-    setInterval(() => {
+    this.mainInterval = setInterval(() => {
       let directionCol = -1;
       let directionRow = 0;
       if (this.direction === 'left') {
@@ -58,15 +63,48 @@ class Grid {
         directionRow = 1;
       }
       this.createGrid();
+      this.randomizeBonus();
       this.snake.unshift([this.snake[0][0] + directionCol, this.snake[0][1] + directionRow]);
-      this.snake.splice(-1, 1);
-      console.log(this.snake);
+      if (this.snake[0][0] !== this.bonusCol || this.snake[0][1] !== this.bonusRow) {
+        this.snake.splice(-1, 1);
+      }
+      if (this.snake[0][0] === this.bonusCol && this.snake[0][1] === this.bonusRow) {
+        this.bonus = false;
+        this.context.beginPath();
+        this.randomizeBonus();
+      }
+      for (let i = 1; i < this.snake.length; i += 1) {
+        if (this.snake[0][0] === this.snake[i][0] && this.snake[0][1] === this.snake[i][1]) {
+          clearInterval(this.mainInterval);
+        }
+      }
+      if (this.snake[0][0] < 0 || this.snake[0][0] > this.tileCol - 1 || this.snake[0][1] < 0 || this.snake[0][1] > this.tileRow - 1) {
+        clearInterval(this.mainInterval);
+      }
+      this.context.fill();
       for (let i = 0; i < this.snake.length; i += 1) {
         this.context.moveTo((this.snake[i][0] + 0.75) * this.tileSize, (this.snake[i][1] + 0.5) * this.tileSize);
         this.context.arc((this.snake[i][0] + 0.5) * this.tileSize, (this.snake[i][1] + 0.5) * this.tileSize, this.tileSize / 4, 0, 2 * Math.PI);
         this.context.stroke();
       }
-    }, 500);
+    }, 200);
+  }
+
+  randomizeBonus() {
+    let isBonusOkay = true;
+    do {
+      if (this.bonus === false) {
+        this.bonusCol = (Math.floor(Math.random() * (this.tileCol)));
+        this.bonusRow = (Math.floor(Math.random() * (this.tileRow)));
+        for (let i = 0; i < this.snake.length; i += 1) {
+          if (this.bonusCol === this.snake[i][0] && this.bonusRow === this.snake[i][1]) {
+            isBonusOkay = false;
+          }
+        }
+        this.bonus = true;
+      }
+    } while (isBonusOkay === false);
+    this.context.arc((this.bonusCol + 0.50) * this.tileSize, (this.bonusRow + 0.5) * this.tileSize, this.tileSize / 4, 0, 2 * Math.PI);
   }
 }
 
